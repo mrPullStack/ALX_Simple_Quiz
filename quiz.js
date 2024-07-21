006e993a95146d78727ef604d250a55e7585b0a6
mrPullStack <cliffland.jasungu@gmail.com>
2024-07-21 21:13:16 +0300

    added event listener to the submit btn

--- quiz.js
+++ quiz.js
@@ -7,5 +7,14 @@ function checkAnswer() {
 
     //get the feedback
     const feedback = document.getElementById("feedback");
-    
+
+    //check user's answer
+    if(userAnswer === correctAnswer){
+        feedback.textContent = "Correct! Well done.";
+    }else {
+        feedback.textContent = "That's incorrect. Try again!"
+    }
 }
+
+// add event listener to the submit btn
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
